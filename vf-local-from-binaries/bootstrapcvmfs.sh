572f21d2e525738e6af4366745c66d26d01b1901
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-06-28 09:39:09 +0100

    Added multiple vagrant configuration

---
+++ vf-local-from-binaries/bootstrapcvmfs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#08/03/2017 tomas - bootstrap from cvmfs, no sources needed
+#17/10/2016 tomas - bootstrap by getting sources from github, cloud version
+/cvmfs/west-life.egi.eu/software/virtualfolder/latest/bootstrap/bootstrapcloud.sh
+echo "BOOTSTRAP FINISHED, VM prepared to use"
