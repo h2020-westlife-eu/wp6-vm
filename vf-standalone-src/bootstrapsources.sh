9d6fe47493b1ba7ac64d086f8fc75f0db86d1630
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-06-20 14:40:18 +0100

    UPDATE: clean bootstrap from sources

--- vf-standalone-src/bootstrapsources.sh
+++ vf-standalone-src/bootstrapsources.sh
@@ -1,47 +1,22 @@
 #!/usr/bin/env bash
-#17/10/2016 tomas - bootstrapt by getting sources from github, cloud version
-
 ##################################################
 # get a wp6 master (3.3 MB) or git clone (21 MB) #
 ##################################################
-
 echo downloading west-life-wp6
 #wget --quiet https://github.com/h2020-westlife-eu/west-life-wp6/archive/master.zip
-#unzip -q master.zip -d /home/vagrant
+#unzip -q master.zip -d /opt/virtualfolder-src
 #rm master.zip
-
 # or alternatively git clone, commented
-cd /home/vagrant
+mkdir -p /opt/virtualfolder-src
+cd /opt/virtualfolder-src
 git clone https://github.com/h2020-westlife-eu/west-life-wp6.git
-
-# optional switch to branch
-cd west-life-wp6
-git checkout dev
-git submodule update --init --recursive
-cd wp6-repository
-git checkout
-git pull origin master
-cd ../..
-
-# rename folder to master - comment if getting zip
-rm -rf west-life-wp6-master
-mv west-life-wp6 west-life-wp6-master
 
 ###########################
 # launch bootstrap script #
 ###########################
 
-export WP6SRC=/home/vagrant/west-life-wp6-master/wp6-virtualfolder
-export WP6REPSRC=/home/vagrant/west-life-wp6-master/wp6-repository
-export PORTAL_DEPLOYMENT=1  # enable VRE - multiuser environment - otherwise single user
+export WP6SRC=/opt/virtualfolder-src/wp6-virtualfolder
+export PORTAL_DEPLOYMENT=0  # enable VRE - multiuser environment - otherwise single user
+export SSO_DEPLOYMENT=1 # enable SSO -multiuser environment
 chown -R vagrant:vagrant $WP6SRC
-ln -s $WP6SRC/bootstrap /home/vagrant/bootstrap
-dos2unix /home/vagrant/bootstrap/*
-chmod ugo+x /home/vagrant/bootstrap/*.sh
-/home/vagrant/bootstrap/bootstrap.sh
-
-chown -R vagrant:vagrant $WP6REPSRC
-dos2unix $WP6REPSRC/*.sh
-chmod ugo+x $WP6REPSRSC/*.sh
-$WP6REPSRC/bootstrap.sh
-
+$WP6SRC/bootstrap/bootstrap.sh
